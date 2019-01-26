89649be59413bbccd7565bb2243907c1dc1f5abc
GregHilston <Gregory.Hilston@gmail.com>
2019-01-25 20:22:31 -0700

    Wrote place holders for generating values for new map from md5 from seed

---
+++ Global-Game-Jam-2019/Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,109 @@
+﻿using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapGenerator : MonoBehaviour
+{
+    private const int roomWidth = 25;
+    private const int roomLength = 25;
+    private const int roomArea = roomWidth * roomLength;
+    private const int minRoomSize = 2;
+    private const int maxRoomSize = 6;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GenerateMap("ANSADOKJASDJASDLJASDOJASODJASODIJASODIJOASIDJASOIJASODIJ");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    /// <summary>
+    /// Creates the MD5 hash of a given input.
+    /// From: https://stackoverflow.com/a/24031467/1983957
+    /// </summary>
+    /// <returns>The MD5 Hash</returns>
+    /// <param name="input">Input stirng to calculate.</param>
+    private string CreateMD5(string input)
+    {
+        // Use input string to calculate MD5 hash
+        using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+        {
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+            // Convert the byte array to hexadecimal string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hashBytes.Length; i++)
+            {
+                sb.Append(hashBytes[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+    }
+
+    private int CalculateNumberOfRooms(string input)
+    {
+        const int indexOfInterest = 0;
+
+        return 6;
+    }
+
+    private int CalculateNumberOfPeopleHome(string input)
+    {
+        const int indexOfInterest = 1;
+
+        return 1;
+    }
+
+    private int CalculateNumberOfPetsHome(string input)
+    {
+        const int indexOfInterest = 2;
+
+        return 1;
+    }
+
+    private int CalculateNumberOfObjectsToMove(string input)
+    {
+        const int indexOfInterest = 3;
+
+        return 3;
+    }
+
+    private int CalculateNumberOfCameras(string input)
+    {
+        const int indexOfInterest = 4;
+
+        return 1;
+    }
+
+    /// <summary>
+    /// Generates the a map from a given seed.
+    /// We leverage hashing the seed to MD5, leveraging the 32 character string.
+    /// We'll interpret each character as hex and each index as follows:
+    /// 0: (int) Room Count =   
+    /// 1: (int) People Home = 
+    /// 2: (int) Number of Pets Home = 
+    /// 3: (int) Number of Objects To Move =
+    /// 4: (int) Number of cameras = 
+    /// 5: (bool) is day time
+    /// </summary>
+    /// <param name="seed">Seed.</param>
+    public void GenerateMap(string seed) 
+    {
+        Debug.Log("seed: " + seed);
+        Debug.Log("md5: " + this.CreateMD5(seed));
+
+        int numberOfRooms = this.CalculateNumberOfRooms(seed);
+        int numberOfPeopleHome = this.CalculateNumberOfPeopleHome(seed);
+        int numberOfPetsHome = this.CalculateNumberOfPetsHome(seed);
+        int numberOfObjectsToMove = this.CalculateNumberOfObjectsToMove(seed);
+        int numberOfCameras = this.CalculateNumberOfCameras(seed);
+    }
+}
+
