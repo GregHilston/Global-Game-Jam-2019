96ffec3910a4295b4ebf0a9e3dee875ca6278ab8
Doug Jacob <djacob9001@gmail.com>
2019-01-26 14:29:46 -0700

    Added new sprite assets to the project; created a system to update player's sprite based on state; created a backup scene, so that if others are editing the SampleScene, I can pull in their branch without overwriting what I've done (scenes don't merge well)

--- Global-Game-Jam-2019/Assets/Scripts/GameManager_Script.cs
+++ Global-Game-Jam-2019/Assets/Scripts/GameManager_Script.cs
@@ -10,7 +10,14 @@ public class GameManager_Script : MonoBehaviour
 	public HingeJoint2D CharGrabJoint;
 	public CircleCollider2D TestGrabBox;
 
+	public HUD_Script HUDScr;
+
+	public Sprite StandingSpr;
+	public Sprite GrabbingSpr;
+	public Sprite[] WalkingSprs;
+
 	bool isGrabbing;
+	bool isWalking;
 	Collider2D[] collArr;
 	ContactFilter2D grabFilter;
 	
@@ -19,6 +26,7 @@ public class GameManager_Script : MonoBehaviour
     {
 		CharGrabJoint.enabled = false;
 		isGrabbing = false;
+		isWalking = false;
 		collArr = new Collider2D[1];
 		grabFilter = new ContactFilter2D();
 		grabFilter.SetLayerMask(LayerMask.GetMask("Grabbable"));
@@ -32,18 +40,32 @@ public class GameManager_Script : MonoBehaviour
 		if (Input.GetKey(KeyCode.RightArrow))
 		{
 			TestChar.GetComponent<Rigidbody2D>().velocity += new Vector2(1f, 0f);
+			//TestChar.GetComponent<Animator>().SetBool("isWalking", !isGrabbing);
+			isWalking = true;
 		}
 		if (Input.GetKey(KeyCode.LeftArrow))
 		{
 			TestChar.GetComponent<Rigidbody2D>().velocity += new Vector2(-1f, 0f);
+			//TestChar.GetComponent<Animator>().SetBool("isWalking", !isGrabbing);
+			isWalking = true;
 		}
 		if (Input.GetKey(KeyCode.UpArrow))
 		{
 			TestChar.GetComponent<Rigidbody2D>().velocity += new Vector2(0f, 1f);
+			//TestChar.GetComponent<Animator>().SetBool("isWalking", !isGrabbing);
+			isWalking = true;
 		}
 		if (Input.GetKey(KeyCode.DownArrow))
 		{
 			TestChar.GetComponent<Rigidbody2D>().velocity += new Vector2(0f, -1f);
+			//TestChar.GetComponent<Animator>().SetBool("isWalking", !isGrabbing);
+			isWalking = true;
+		}
+		if (!Input.GetKey(KeyCode.DownArrow) && !Input.GetKey(KeyCode.UpArrow) &&
+			!Input.GetKey(KeyCode.LeftArrow) && !Input.GetKey(KeyCode.RightArrow))
+		{
+			//TestChar.GetComponent<Animator>().SetBool("isWalking", false);
+			isWalking = false;
 		}
 
 		//if (Input.GetKeyDown(KeyCode.A) && (TestObj.transform.position - TestChar.transform.position).magnitude <= 1.5f)
@@ -52,26 +74,52 @@ public class GameManager_Script : MonoBehaviour
 			CharGrabJoint.enabled = true;
 			CharGrabJoint.connectedBody = collArr[0].GetComponent<Rigidbody2D>();
 			isGrabbing = true;
+
+			TestChar.GetComponent<SpriteRenderer>().sprite = GrabbingSpr;
 		}
-		if (Input.GetKeyUp(KeyCode.A))
+		if (Input.GetKeyUp(KeyCode.A) && isGrabbing)
 		{
 			CharGrabJoint.enabled = false;
 			isGrabbing = false;
+
+			TestChar.GetComponent<SpriteRenderer>().sprite = StandingSpr;
 		}
 
 		if (isGrabbing)
 		{
 			Vector2 tmpPos = TestObj.transform.position - TestChar.transform.position;
 			TestChar.transform.localRotation = Quaternion.Euler(0f, 0f, Mathf.Rad2Deg * Mathf.Atan2(tmpPos.y, tmpPos.x));
+
+			if (Input.GetKeyDown(KeyCode.S))
+			{
+				CharGrabJoint.connectedBody.AddForce(
+					(CharGrabJoint.connectedBody.transform.position - TestChar.transform.position).normalized * 10000f);
+
+				CharGrabJoint.enabled = false;
+				isGrabbing = false;
+
+				TestChar.GetComponent<SpriteRenderer>().sprite = StandingSpr;
+			}
 		}
-		else if (TestChar.GetComponent<Rigidbody2D>().velocity.SqrMagnitude() > 0f)
+		else
 		{
-			Vector2 tmpVec = TestChar.GetComponent<Rigidbody2D>().velocity;
-			//Vector2 tmpPos = TestObj.transform.position - TestChar.transform.position;
-			TestChar.transform.localRotation = Quaternion.RotateTowards(
-				TestChar.transform.rotation,
-				Quaternion.Euler(0f, 0f, Mathf.Rad2Deg * Mathf.Atan2(tmpVec.y, tmpVec.x)),
-				4f);
+			if (TestChar.GetComponent<Rigidbody2D>().velocity.SqrMagnitude() > 0f)
+			{
+				Vector2 tmpVec = TestChar.GetComponent<Rigidbody2D>().velocity;
+				//Vector2 tmpPos = TestObj.transform.position - TestChar.transform.position;
+				TestChar.transform.localRotation = Quaternion.RotateTowards(
+					TestChar.transform.rotation,
+					Quaternion.Euler(0f, 0f, Mathf.Rad2Deg * Mathf.Atan2(tmpVec.y, tmpVec.x)),
+					5f);
+			}
+			if (isWalking)
+			{
+				TestChar.GetComponent<SpriteRenderer>().sprite = WalkingSprs[HUDScr.Timer % 32 / 8];
+			}
+			else
+			{
+				TestChar.GetComponent<SpriteRenderer>().sprite = StandingSpr;
+			}
 		}
 
 //#endif
