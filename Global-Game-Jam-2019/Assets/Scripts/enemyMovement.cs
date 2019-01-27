6d3a524579082ab0eacba5529f02d931f1552832
downhillGames <downhillgamesofficial@gmail.com>
2019-01-27 07:28:00 -0800

    messed with enemy AI in sample scene and added high score to end game screen

--- Global-Game-Jam-2019/Assets/Scripts/enemyMovement.cs
+++ Global-Game-Jam-2019/Assets/Scripts/enemyMovement.cs
@@ -5,176 +5,68 @@ using UnityEngine;
 public class enemyMovement : MonoBehaviour
 {
     private float speed = 4.0f;
-    public const int middleX = 0;
-    public const int middleY =0;
-    public const int middle2X = 5;
-    public const int middle2Y = 5;
-    public const int middle3X = -5;
-    public const int middle3Y = -5;
 
-    Vector2[] gridPoints =
-    {
-        //room 1
-        new Vector2(middleX+2, middleY+2),
-        new Vector2(middleX-2, middleY-2),
-        new Vector2(middleX+2, middleY+2),
-        new Vector2(middleX+2, middleY-2),
-        new Vector2(middleX,  middleY-2),
-        
-        //room2
-        new Vector2(middle2X+2, middle2Y+2),
-        new Vector2(middle2X-2, middle2Y-2),
-        new Vector2(middle2X+2, middle2Y+2),
-        new Vector2(middle2X+2, middle2X-2),
-        new Vector2(middle2X, middle2Y-2),
 
-         //room3
-        new Vector2(middle3X+2, middle3Y+2),
-        new Vector2(middle3X-2, middle3Y-2),
-        new Vector2(middle3X+2, middle3Y+2),
-        new Vector2(middle3X+2, middle3X-2),
-        new Vector2(middle3X, middle3Y-2)
 
-    };
-    
-    //declare global variables
-    public Vector2 target;
-    public Vector2  targetPoint;
-    private Vector2 position;
-    private bool onGridPoint;
-    private bool onTargetPoint;
-    public int arrayPosition = 0;
-    public int rand;
-    public int targetArray;
-    public int gridPointRoomNumber;
-    public int playerLocation;
+    //declare global variable
+    public float rand1, rand2, rand3, timer;
+    public Vector2 position;
+ 
 
     // Start is called before the first frame update
     void Start()
-    {       
-        DecideGridPoint();
+    {
+
         position = gameObject.transform.position;
+        gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(0.0f, 0.0f);
+        Randomize();
     }
 
-    // Update is called once per frame
     void Update()
     {
-
-        //update game enemy position
         position = gameObject.transform.position;
+
+        if (position.x >= -6 && position.x <= 6 && position.y >= -4.5 && position.y <= 4.5)
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(rand1, rand2);
+        }
+        else if ((position.x >= -6 && position.x <= 6) && ( position.y < -4.5 || position.y > 4.5))
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(rand1, -rand2);
+        }
+        else if ((position.x < -6 || position.x > 6) && (position.y >= -4.5 || position.y <= 4.5))
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(-rand1, rand2);
+        }
+
+
+
+        if (timer > 0)
+        {
+
+            timer--;
+        }
+        else if(timer <= 0)
+        {
+            Randomize();
+        }
+    }
+    void DecideDestination()
+    {
+
+    }
+
+
+    void Randomize()
+    {
+        rand1 = Random.Range(-5.0f, 5.0f);
+        rand2 = Random.Range(-5.0f, 5.0f);
+        rand3 = Random.Range(5.0f, 10.0f);
+        timer = rand3;
         
-        //enemy speed
-        float step = speed * Time.deltaTime;
-
-        //move to points on grid
-        if (position != targetPoint && position != target)
-        {
-            transform.position = Vector2.MoveTowards(transform.position, target, step);
-        }
-        else if (position==target && position!=targetPoint)
-        {
-           target = targetPoint;
-        }
-        else if (position == targetPoint)
-        {
-            DecideGridPoint();
-        }
-   
-
-    }
-
-    //decide target point
-    void DecideTargetPoint()
-    { 
-        rand = Random.Range(0, 15);
-        targetArray = rand;
-        targetPoint = gridPoints[rand];
-    }
-
-    //decide temporary target
-    void DecideGridPoint()
-    {
-        DecideTargetPoint();
-        
-        int currentRoom = getCurrentRoomNumber();
-        int targetRoom = getTargetRoomNumber();
-
-        if (currentRoom != targetRoom && currentRoom == 1)
-        {
-            target = gridPoints[4];
-        }
-        else if (currentRoom != targetRoom && currentRoom == 2)
-        {
-            target = gridPoints[9];
-        }
-        else
-        {
-            target = targetPoint;
-        }
-
-    }
-
-    public int getTargetRoomNumber()
-    {
-        int numberOfRooms = 3;
-        int index = 0;
-        int dataPoints = numberOfRooms * 2;
-
-        while (index < dataPoints)
-        {
-            if (targetPoint == gridPoints[index] && index < 5)
-            {
-                return 1;
-            }
-            else if (targetPoint == gridPoints[index] && index >= 5 && index < 10)
-            {
-                return 2;
-            }
-            else if (targetPoint == gridPoints[index] && index >= 10 && index < 15)
-            {
-                return 3;
-            }
-            else if (targetPoint == gridPoints[index] && index >= 15 && index < 20)
-            {
-                return 4;
-            }
-            index++;
-
-        }
-        return 0; ///SHOULD NOT HIT THIS CODE
-    }
-
-
-    public int getCurrentRoomNumber()
-    {
-        int numberOfRooms = 3;
-        int index = 0;
-        int dataPoints = numberOfRooms * 2;
-
-        while (index < dataPoints)
-        {
-            if (position == gridPoints[index] && index < 5)
-            {
-                return 1;
-            }
-            else if (position == gridPoints[index] && index >= 5 && index < 10)
-            {
-                return 2;
-            }
-            else if (position == gridPoints[index] && index >= 10 && index < 15)
-            {
-                return 3;
-            }
-            else if (position == gridPoints[index] && index >= 15 && index < 20)
-            {
-                return 4;
-            }
-            index++;
-            
-        }
-        return 0; ///SHOULD NOT HIT THIS CODE
     }
 
 }
+   
 
 
